2fe287b3807d4b83fc0a1504079a243d7409e6af
Marius <mariussangwa@gmail.com>
2026-01-10 21:20:55 +0200

    adding grids to projects page

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,10 +1,13 @@
 import Project from '../components/Projects';
-import code from '../assets/coder.webp'
+import piezo from '../assets/piezo.jpeg'
+import backup from '../assets/backup.jpeg'
 
 function Projects(){
     return(
         <>
-       <Project projectTitle='Piezo-electricity' projectPhoto={code} projectDescription={'This is a project about generating electric power from motion like foot'}/>
+       <Project projectTitle='Piezo-electricity' projectPhoto={piezo} projectDescription={'This is a project about generating electric power from motion like foot-steps'}/>
+       <Project projectTitle='Back up bash script' projectPhoto={backup} projectDescription='This is a complete project where you can be able to backup your folders before deletion, and you can find it on my git hub'/>
+       <Project />
         </>
     )
 }
