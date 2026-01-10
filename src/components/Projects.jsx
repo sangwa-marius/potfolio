263c851ec31150d301e64dc8b7455782efbdbaa9
Marius <mariussangwa@gmail.com>
2026-01-10 22:02:06 +0200

    adjusting many styles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,13 @@
 function Project({projectTitle, projectPhoto, projectDescription}){
     return(
         <>
-            <div className ='projects'>
-                <div className='project-card'>
+    
+            <div className='project-card'>
                 <h2>{projectTitle}</h2>
                 <div><img src={projectPhoto}/></div>
                 <p>{projectDescription}</p>
             </div>
-            </div>
+            
             
         </>
     )
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -5,9 +5,12 @@ import backup from '../assets/backup.jpeg'
 function Projects(){
     return(
         <>
+        <div className="projects">
+
        <Project projectTitle='Piezo-electricity' projectPhoto={piezo} projectDescription={'This is a project about generating electric power from motion like foot-steps'}/>
        <Project projectTitle='Back up bash script' projectPhoto={backup} projectDescription='This is a complete project where you can be able to backup your folders before deletion, and you can find it on my git hub'/>
        <Project />
+        </div>
         </>
     )
 }
